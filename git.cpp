00743ca8e6a5201c95b6c270d4b65b40d8410efc
Anton <fakinfa@rambler.ru>
2023-05-19 19:45:32 +0300

    create func init_vector

--- git.cpp
+++ git.cpp
@@ -2,10 +2,26 @@
 //
 
 #include <iostream>
+using namespace std;
+struct vector {
+    int* array;
+    unsigned int size;
+};
+vector init_vector(unsigned int size) {
+    vector mas;
+    mas.array = new int[size];
+    mas.size = size;
+
+    for (unsigned int i = 0; i < size; ++i) {
+        mas.array[i] = rand() % 56;
+
+    }
+    return mas;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector mas = init_vector(4);
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
